a9c135b68aa76fafe6afbfdd66dd0198665ae3ee
Jochen Voss <voss@seehuhn.de>
2023-05-30 09:19:14 +0100

    updates

--- data/set.go
+++ data/set.go
@@ -8,3 +8,25 @@ type Set interface {
 	TrainingData() (data *Data, err error)
 	TestData() (data *Data, err error)
 }
+
+func MakeSet(name string, train, test *Data) Set {
+	return &set{name, train, test}
+}
+
+type set struct {
+	name  string
+	train *Data
+	test  *Data
+}
+
+func (s *set) GetName() string {
+	return s.name
+}
+
+func (s *set) TrainingData() (data *Data, err error) {
+	return s.train, nil
+}
+
+func (s *set) TestData() (data *Data, err error) {
+	return s.test, nil
+}
